2707c95400808672d058e37a8c884c22b327c34d
Chidimma Nworah <78983600+chidimmaNworah@users.noreply.github.com>
2022-11-10 10:51:30 +0100

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({children}) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Puffizzy Smoke Assessories store</title>
+        <link rel="shortcut icon" href="/puffizzy_logo.png" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
